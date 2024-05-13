744ef7eb65c7770693cfea36b1d02a24ee62dcca
Gabriel-BMarques <gabrielbutkus256@gmail.com>
2024-05-13 01:26:32 -0300

    chore: added cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
+import { NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use((req, res, next) => {
+  app.use(cors());
+
+  app.use((req: Request, _res: Response, next: NextFunction) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
   });
 
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
